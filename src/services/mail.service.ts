d1df4ce4fd673a8550dc2a1d9f736649928b7e25
Sachin Singh Tomar <sachinsinghtomar912@gmail.com>
2026-05-18 10:49:09 +0530

    updated code for email send

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -5,10 +5,11 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: true,
+  port: Number(process.env.SMTP_PORT || 465),
+  secure: Number(process.env.SMTP_PORT) === 465,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 20000,
 });
